32cfea20cad9c8dd83f6f0425bf0fb9dc543ae82
JT Creates <84053338+JT-Creates@users.noreply.github.com>
2021-06-27 14:41:10 -0400

    Create test.ino

---
+++ LiquidCrystal4004/examples/test.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystalDualController.h>
+
+const int en1 = 12, en2 = 7; LiquidCrystalDualController lcd(13, en1, en2, 11, 10, 9, 8);
+void setup() {
+    lcd.begin(40,4);
+ // lcd.print("hello, world! ");
+}
+
+void loop() {
+   // lcd1.setCursor(0,0);
+    lcd.print("Hello, there!        ");
+    delay(1);
+    lcd.print("This is a test!      ");
+    delay(1);
+    lcd.print("Testing 4004.        ");
+    delay(1);
+    lcd.print("Did it work?         ");
+    delay(1000);
+}
