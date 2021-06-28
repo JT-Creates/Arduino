9be1312df5f368157969d70307e24bedff3084b3
JT Creates <84053338+JT-Creates@users.noreply.github.com>
2021-06-28 15:31:36 -0400

    LiquidCrystal4004 v2.3.0
    A partially working display driver.

--- libraries/LiquidCrystal4004/src/LiquidCrystal4004.cpp
+++ libraries/LiquidCrystal4004/src/LiquidCrystal4004.cpp
@@ -1,5 +1,4 @@
 #include "LiquidCrystal4004.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
@@ -24,8 +23,7 @@
 // can't assume that its in that state when a sketch starts (and the
 // LiquidCrystal constructor is called).
 
-double cur_col = 0;
-int col_override = 0;
+int cur_col = 0, col_override = 0;
 
 int digitalReadOutputPin(uint8_t pin) {
   uint8_t bit = digitalPinToBitMask(pin);
@@ -37,15 +35,12 @@ int digitalReadOutputPin(uint8_t pin) {
 LiquidCrystal4004::LiquidCrystal4004(uint8_t rs, uint8_t rw, uint8_t enable1, uint8_t enable2, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) {
   init(0, rs, rw, enable1, enable2, d0, d1, d2, d3, d4, d5, d6, d7);
 }
-
 LiquidCrystal4004::LiquidCrystal4004(uint8_t rs, uint8_t enable1, uint8_t enable2, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) {
   init(0, rs, 255, enable1, enable2, d0, d1, d2, d3, d4, d5, d6, d7);
 }
-
 LiquidCrystal4004::LiquidCrystal4004(uint8_t rs, uint8_t rw, uint8_t enable1, uint8_t enable2, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3) {
   init(1, rs, rw, enable1, enable2, d0, d1, d2, d3, 0, 0, 0, 0);
 }
-
 LiquidCrystal4004::LiquidCrystal4004(uint8_t rs, uint8_t enable1, uint8_t enable2, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3) {
   init(1, rs, 255, enable1, enable2, d0, d1, d2, d3, 0, 0, 0, 0);
 }
@@ -58,33 +53,31 @@ void LiquidCrystal4004::init(uint8_t fourbitmode, uint8_t rs, uint8_t rw, uint8_
   _data_pins[0] = d0;
   _data_pins[1] = d1;
   _data_pins[2] = d2;
-  _data_pins[3] = d3; 
+  _data_pins[3] = d3;
   _data_pins[4] = d4;
   _data_pins[5] = d5;
   _data_pins[6] = d6;
-  _data_pins[7] = d7; 
+  _data_pins[7] = d7;
 
   pinMode(_rs_pin, OUTPUT);
   // we can save 1 pin by not using RW. Indicate by passing 255 instead of pin#
   if (_rw_pin != 255) pinMode(_rw_pin, OUTPUT);
   pinMode(_enable_pin1, OUTPUT);
   pinMode(_enable_pin2, OUTPUT);
-  
   if (fourbitmode) _displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
   else _displayfunction = LCD_8BITMODE | LCD_1LINE | LCD_5x8DOTS;
-  
-  begin(16, 1);  
+  begin(40, 4);  
 }
 
 void LiquidCrystal4004::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
   cur_col = 0;
-  if (lines == 2) _displayfunction |= LCD_2LINE;
+  if (lines > 1) _displayfunction |= LCD_2LINE;
   _numlines = lines;
 
   setRowOffsets(0x00, 0x40, 0x00 + cols, 0x40 + cols);  
 
   // for some 1 line displays you can select a 10 pixel high font
-  if ((dotsize != LCD_5x8DOTS) && (lines == 1)) _displayfunction |= LCD_5x10DOTS;
+  if ((dotsize != LCD_5x8DOTS) & (lines == 1)) _displayfunction |= LCD_5x10DOTS;
 
   // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
   // according to datasheet, we need at least 40ms after power rises above 2.7V
@@ -92,8 +85,8 @@ void LiquidCrystal4004::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
   delayMicroseconds(50000); 
   // Now we pull both RS and R/W low to begin commands
   digitalWrite(_rs_pin, LOW);
-  digitalWrite(_enable_pin1, LOW);
-  digitalWrite(_enable_pin2, LOW);
+  //digitalWrite(_enable_pin1, LOW);
+  //digitalWrite(_enable_pin2, LOW);
   if (_rw_pin != 255) digitalWrite(_rw_pin, LOW);
   
   //put the LCD into 4 bit or 8 bit mode
@@ -130,6 +123,7 @@ void LiquidCrystal4004::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
     write4bits(0x02);
     col_override = 2;
     write4bits(0x02);
+    col_override = 0;
   } else {
     // this is according to the hitachi HD44780 datasheet
     // page 45 figure 23
@@ -158,6 +152,7 @@ void LiquidCrystal4004::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
 
   // Initialize to default text direction (for romance languages)
   _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+
   // set the entry mode
   command(LCD_ENTRYMODESET | _displaymode);
 }
@@ -170,30 +165,23 @@ void LiquidCrystal4004::setRowOffsets(int row0, int row1, int row2, int row3) {
 }
 
 /********** high level commands, for the user! */
-void LiquidCrystal4004::clear()
-{
+void LiquidCrystal4004::clear() {
   command(LCD_CLEARDISPLAY);  // clear display, set cursor position to zero
   delayMicroseconds(2000);  // this command takes a long time!
-  //cur_col = 0;
+ // cur_col = 0;
+ // col_override = 0;
 }
 
-void LiquidCrystal4004::home()
-{
+void LiquidCrystal4004::home() {
   command(LCD_RETURNHOME);  // set cursor position to zero
   delayMicroseconds(2000);  // this command takes a long time!
   //cur_col = 0;
 }
 
-void LiquidCrystal4004::setCursor(uint8_t col, uint8_t row)
-{
+void LiquidCrystal4004::setCursor(uint8_t col, uint8_t row) {
   const size_t max_lines = sizeof(_row_offsets) / sizeof(*_row_offsets);
-  if ( row >= max_lines ) {
-    row = max_lines - 1;    // we count rows starting w/0
-  }
-  if ( row >= _numlines ) {
-    row = _numlines - 1;    // we count rows starting w/0
-  }
-  
+  if (row >= max_lines) row = max_lines - 1;    // we count rows starting w/0
+  if (row >= _numlines) row = _numlines - 1;    // we count rows starting w/0
   //cur_col = col * row;
   command(LCD_SETDDRAMADDR | (col + _row_offsets[row]));
 }
@@ -265,37 +253,25 @@ void LiquidCrystal4004::noAutoscroll(void) {
 void LiquidCrystal4004::createChar(uint8_t location, uint8_t charmap[]) {
   location &= 0x7; // we only have 8 locations 0-7
   command(LCD_SETCGRAMADDR | (location << 3));
-  for (int i=0; i<8; i++) {
-    write(charmap[i]);
-  }
+  for (int i=0; i<8; i++) write(charmap[i]);
 }
 
 /*********** mid level commands, for sending data/cmds */
 
 inline void LiquidCrystal4004::command(uint8_t value) {
   digitalWrite(_rs_pin, LOW);
-
   // if there is a RW pin indicated, set it low to Write
-  if (_rw_pin != 255) { 
-    digitalWrite(_rw_pin, LOW);
-  }
-  col_override = 1;
-  if (_displayfunction & LCD_8BITMODE) {
-    write8bits(value); 
-  } else {
+  if (_rw_pin != 255) digitalWrite(_rw_pin, LOW);
+  col_override++;
+  if (!(_displayfunction & LCD_8BITMODE)) 
     write4bits(value>>4);
-    write4bits(value);
-  }
-  col_override = 2;
-  if (_displayfunction & LCD_8BITMODE) {
-    write8bits(value); 
-  } else {
+  write4bits(value);
+  col_override++;
+  if (!(_displayfunction & LCD_8BITMODE)) 
     write4bits(value>>4);
-    write4bits(value);
-  }
+  write4bits(value);
   col_override = 0;
 }
-
 inline size_t LiquidCrystal4004::write(uint8_t value) {
   send(value, HIGH);
   return 1; // assume sucess
@@ -305,18 +281,16 @@ inline size_t LiquidCrystal4004::write(uint8_t value) {
 
 // write either command or data, with automatic 4/8-bit selection
 void LiquidCrystal4004::send(uint8_t value, uint8_t mode) {
+  cur_col++;
+  if (cur_col > 160) cur_col = 1;
+  Serial.println("col: ");
+  Serial.println(cur_col);
   digitalWrite(_rs_pin, mode);
-
   // if there is a RW pin indicated, set it low to Write
-  if (_rw_pin != 255) { 
-    digitalWrite(_rw_pin, LOW);
-  }
-  if (_displayfunction & LCD_8BITMODE) {
-    write8bits(value); 
-  } else {
+  if (_rw_pin != 255) digitalWrite(_rw_pin, LOW);
+  if (!(_displayfunction & LCD_8BITMODE)) 
     write4bits(value>>4);
-    write4bits(value);
-  }
+  write4bits(value);
 }
 
 void LiquidCrystal4004::pulseEnable(uint8_t _e_pin) {
@@ -326,11 +300,6 @@ void LiquidCrystal4004::pulseEnable(uint8_t _e_pin) {
   delayMicroseconds(1);
   digitalWrite(_e_pin, LOW);
   delayMicroseconds(37); // commands need > 37us to settle
-  if (cur_col > 160) {
-    cur_col = 0;
-  } else {
-    cur_col++;
-  }
 }
 
 void LiquidCrystal4004::write4bits(uint8_t value) {
@@ -338,19 +307,10 @@ void LiquidCrystal4004::write4bits(uint8_t value) {
     pinMode(_data_pins[i], OUTPUT);
     digitalWrite(_data_pins[i], (value >> i) & 0x01);
   }
-  if (col_override == 0) {
-    if (cur_col < 80) {
-      pulseEnable(_enable_pin1);
-    } else {
-      pulseEnable(_enable_pin2);
-    }
-  } else if (col_override == 1) {
+  if ((cur_col <= 80 & col_override == 0) | col_override == 1)
     pulseEnable(_enable_pin1);
-    cur_col--;
-  } else if (col_override == 2) {
+  else if ((cur_col > 80 & col_override == 0) | col_override == 2)
     pulseEnable(_enable_pin2);
-    cur_col--;
-  }
 }
 
 void LiquidCrystal4004::write8bits(uint8_t value) {
@@ -358,17 +318,8 @@ void LiquidCrystal4004::write8bits(uint8_t value) {
     pinMode(_data_pins[i], OUTPUT);
     digitalWrite(_data_pins[i], (value >> i) & 0x01);
   }
-  if (col_override == 0) {
-    if (cur_col < 80) {
-      pulseEnable(_enable_pin1);
-    } else {
-      pulseEnable(_enable_pin2);
-    }
-  } else if (col_override == 1) {
+  if ((cur_col <= 80 & col_override == 0) | col_override == 1)
     pulseEnable(_enable_pin1);
-    cur_col--;
-  } else if (col_override == 2) {
+  else if ((cur_col > 80 & col_override == 0) | col_override == 2)
     pulseEnable(_enable_pin2);
-    cur_col--;
-  }
 }
--- test/test.ino
+++ test/test.ino
@@ -2,39 +2,43 @@
 
 LiquidCrystal4004 lcd(13, 12, 7, 11, 10, 9, 8);
 void setup() {
-    lcd.begin(40,4);
-    lcd.print("Hello, there!       ");
-    delay(100);
-    lcd.print("                    ");
-    delay(100);
-    lcd.print("This is a test!     ");
-    delay(100);
-    lcd.print("                    ");
-    delay(100);
-   // lcd.print("  ");
-   // lcd.print("hello, world! ABCDEFGHIJKLMNOPQRSTUVWXYZ acbdefghijklmnopqrstuvwxyz 1234567890 `~!@#$%^&*()_+[]|}{:'\"\\/.,?>< ");
+  Serial.begin(9600);
+  lcd.begin(40,4);
+ // lcd.print("Hello, there!       " + "                    ");
+ // lcd.print("This is a test!     " + "                    ");
+  delay(100);
+ // lcd.print("  ");
+ // lcd.print("hello, world! ABCDEFGHIJKLMNOPQRSTUVWXYZ acbdefghijklmnopqrstuvwxyz 1234567890 `~!@#$%^&*()_+[]|}{:'\"\\/.,?>< ");
 }
 
 void loop() {
-   // lcd.print(" ");
-   // lcd.setCursor(30,3);
-   //          012345678901234567890
-   //          ---------------------
-   // delay(2000);
-    lcd.print("Testing row 1.      ");
-    delay(10);
-    lcd.print("Did row 1 work?     ");
-    delay(1000);
-    lcd.print("Testing row 2.      ");
-    delay(10);
-    lcd.print("Did row 2 work?     ");
-    delay(1000);
-    lcd.print("Testing row 3.      ");
-    delay(10);
-    lcd.print("Did row 3 work?     ");
-    delay(1000);
-    lcd.print("Testing row 4.      ");
-    delay(10);
-    lcd.print("Did row 4 work?     ");
-   // delay(2000);
+ // lcd.print(" ");
+ // lcd.setCursor(30,3);
+ //          012345678901234567890
+ //          ---------------------
+  delay(200);
+  lcd.print("Testing row 1.      ");
+  lcd.print("Did row 1 work?     ");
+  Serial.print("----");
+  lcd.print("Testing row 2.      ");
+  lcd.print("Did row 2 work?     ");
+  Serial.print("----");
+  lcd.print("Testing row 3.      ");
+  lcd.print("Did row 3 work?     ");
+  Serial.print("----");
+  lcd.print("Testing row 4.      ");
+  lcd.print("Did row 4 work?     ");
+  Serial.print("----");
+ // delay(200);
+  //text();
+}
+
+void text(){
+  String text = Serial.readString();
+  for(int i = 0; i < text.length(); i++) {
+    //here you could check the servo number
+    String pos = text.substring(i, i + 1);
+    Serial.println("Pos: " + i);
+    Serial.println(pos);
+  }
 }
