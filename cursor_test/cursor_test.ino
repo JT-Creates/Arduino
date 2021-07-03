4a5e1ddfcc273bcc40ad4704a6e52b338c6ff7e5
JT Creates <84053338+JT-Creates@users.noreply.github.com>
2021-07-03 17:50:13 -0400

    Update cursor_test.ino

--- cursor_test/cursor_test.ino
+++ cursor_test/cursor_test.ino
@@ -4,12 +4,12 @@ LiquidCrystal4004 lcd(13, 12, 7, 11, 10, 9, 8);
 int JoyStick_X = 0; //x
 int JoyStick_Y = 1; //y
 int JoyStick_Z = 3; //key
-int z = 0, x, y, cx = 0, cy = 0;
+int z = 0, dx = 0, dy = 0, x, y, cx = 0, cy = 0;
 void setup(){
   pinMode(JoyStick_Z, INPUT); 
   Serial.begin(9600); // 9600 bps
-  //cx = analogRead(JoyStick_X);
-  //cy = analogRead(JoyStick_Y);
+  cx = analogRead(JoyStick_X);
+  cy = analogRead(JoyStick_Y);
   lcd.begin(40, 4);
   lcd.cursor();
   lcd.blink();
@@ -25,18 +25,34 @@ void loop() {
   //         ----------------------------------------
   x = analogRead(JoyStick_X);
   y = analogRead(JoyStick_Y); 
-  if (x > 10 | y > 10 | x < -10 | y < -10){
-    int dir = JoyStickDirection();
-    Serial.println(dir);
+  dx = x - cx;
+  dy = y - cy;
+  Serial.print("x: ");
+  Serial.println(x);
+  Serial.print("y: ");
+  Serial.println(y);
+  if (x > (cx+4) | y > (cy+4) | x < (cx-4) | y < (cy-4)){
+  int dir = JoyStickDirection();
+  Serial.println(dir);
     if (dir == 1) lcd.goto_Cell(lcd.cursorPos() + 1);
     else if (dir == 3) lcd.goto_Cell(lcd.cursorPos() - 1);
-    else if (dir == 2) lcd.goto_Cell(lcd.cursorPos() + 40);
-    else if (dir == 4) lcd.goto_Cell(lcd.cursorPos() - 40);
-    delay((abs(x)+1)/(abs(y)+1)*10);
-    //Serial.print(lcd.cursorPos());
-  }
+    else if (dir == 2) {
+      for(int i = 0; i < 40; i++){
+        lcd.goto_Cell(lcd.cursorPos() + 1);
+      }
+    }
+    else if (dir == 4) {
+      for(int i = 0; i < 40; i++){
+        lcd.goto_Cell(lcd.cursorPos() - 1);
+      }
+    }
+    delay(100);
+    //delay(100+((abs(dx+1)/abs(dy+1))+1));
+  } else {
     cx = analogRead(JoyStick_X);
     cy = analogRead(JoyStick_Y);
+  }
+  delay(100);
 }
 
 void charSelect(){
@@ -49,16 +65,16 @@ void charSelect(){
 }
 
 int JoyStickDirection(){
-  if (x > y){
-    if (x > 1) {
+  if (x > (y+4) & dx > (dy+4)){
+    if (x > (cx+2)) {
       return 1;
-    } else if (x < -1) {
+    } else if (x < (cx-2)) {
       return 3;
     }
-  } else if (y > x) {
-    if (y > 1) {
+  } else if (y > (x+4) & dy > (dx+4)) {
+    if (y > (cy+2)) {
       return 2;
-    } else if (y < -1) {
+    } else if (y < (cy-2)) {
       return 4;
     }
   }
